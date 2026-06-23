771249fc9f0c2100d8f05e67b9064d2d7e804411
Max Marriott-Clarke <maxmarriottclarke@me.com>
2026-06-23 14:24:33 +0200

    Removed old code

--- 26-CPP/exercise-example/nbody/verify.h
+++
@@ -1,5 +0,0 @@
-#define VERIFY(expr)                                 \
-  do {                                               \
-    if (not (expr)) throw "failure at: `" #expr "`"; \
-  } while (false)
-
