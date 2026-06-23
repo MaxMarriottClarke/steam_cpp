977a9adf3197109d743fb00efefa0631a6cc80c6
Max Marriott-Clarke <maxmarriottclarke@me.com>
2026-06-23 15:56:58 +0200

    Tidied up issues with inputs being undefined

--- src/exercises/histograms/main.cpp
+++ src/exercises/histograms/main.cpp
@@ -8,29 +8,31 @@ int main () {
 
   // Allow user to input N
   int n;
+  std::println("Number of samples to produce, N =");
   std::cin >> n;
 
   if (!std::cin || n < 0) return 1;
 
 
   // initialise the memory
-  std::vector<float> data(n);
+  std::vector<float> data;
+  data.reserve(n);
 
   //random number generator
   //add N random numbers to data
-  //data~N(0,1)
-  
+  //data~N(0,1) 
   std::random_device rd;
   std::philox4x64 rne(rd());
   std::normal_distribution norm{0.0, 1.0};
 
-  for (float& e : data) {
-    e = norm(rne);
+  for (int i = 0; i < n; i++) {
+    auto entry = norm(rne);
+    data.emplace_back(entry);
+    std::println("{}", entry);
   }
 
-  for (auto& i : data) {
-    std::println("{}", i);
-  }
+
+
 
 
 }
